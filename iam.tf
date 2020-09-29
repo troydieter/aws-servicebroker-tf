6a759a8d8e3524482efeb083b943279d922593bc
Troy Dieter <troydieter@gmail.com>
2020-09-28 20:31:20 -0400

    moved to seperated policy

--- iam.tf
+++ iam.tf
@@ -1,5 +1,14 @@
 data "aws_caller_identity" "current" {}
 
+ data "template_file" "serv-broker-policy" {
+   template = file("serv-broker-policy.json")
+
+   vars = {
+     aws_region = var.aws_region
+     aws_account = data.aws_caller_identity.current.account_id
+   }
+ }
+
 resource "random_id" "iam-rand" {
   byte_length = 6
 }
@@ -11,95 +20,7 @@ resource "aws_iam_user" "serv-broker-user" {
 resource "aws_iam_policy" "serv-broker_policy" {
   name        = "serv-broker_policy"
   description = "Policy that is to be attached to the PCF Service Broker User"
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "SsmForSecretBindings",
-        "Action": "ssm:PutParameter",
-        "Resource": "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/asb-*",
-        "Effect": "Allow"
-      },
-      {
-        "Sid": "AllowCfnToGetTemplates",
-        "Action": "s3:GetObject",
-        "Resource": "arn:aws:s3:::awsservicebroker/templates/*",
-        "Effect": "Allow"
-      },
-      {
-         "Sid": "CloudFormation",
-         "Action": [
-            "cloudformation:CreateStack",
-            "cloudformation:DeleteStack",
-            "cloudformation:DescribeStacks",
-            "cloudformation:DescribeStackEvents",
-            "cloudformation:UpdateStack",
-            "cloudformation:CancelUpdateStack"
-         ],
-         "Resource": [
-            "arn:aws:cloudformation:${var.aws_region}:${data.aws_caller_identity.current.account_id}:stack/aws-service-broker-*/*"
-         ],
-         "Effect": "Allow"
-      },
-     {
-        "Sid": "ServiceClassPermissions",
-        "Action": [
-           "athena:*",
-           "dynamodb:*",
-           "kms:*",
-           "elasticache:*",
-           "elasticmapreduce:*",
-           "kinesis:*",
-           "rds:*",
-           "redshift:*",
-           "route53:*",
-           "s3:*",
-           "sns:*",
-           "sqs:*",
-           "ec2:*",
-           "iam:*",
-           "lambda:*"
-        ],
-        "Resource": [
-           "*"
-        ],
-        "Effect": "Allow"
-     },
-     {
-        "Action": [
-           "s3:GetObject",
-           "s3:ListBucket"
-        ],
-        "Resource": [
-           "arn:aws:s3:::awsservicebroker/templates/*",
-           "arn:aws:s3:::awsservicebroker"
-        ],
-        "Effect": "Allow"
-      },
-      {
-        "Action": [
-          "dynamodb:PutItem",
-          "dynamodb:GetItem",
-          "dynamodb:DeleteItem"
-        ],
-        "Resource": "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/{aws_dynamodb_table.brokertable.id}",
-        "Effect": "Allow"
-      },
-      {
-        "Action": [
-          "ssm:GetParameter",
-          "ssm:GetParameters"
-        ],
-        "Resource": [
-            "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/asb-*",
-            "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/Asb*"
-        ],
-        "Effect": "Allow"
-      }
-   ]
-}
-EOF
+  policy      = data.template_file.serv-broker-policy.rendered
 }
 
 resource "aws_iam_policy_attachment" "serv-broker_policy" {
